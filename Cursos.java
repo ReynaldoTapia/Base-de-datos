de7a32e820fd66adcd6706569c36d775a3cea92f
ReynaldoTapia <u24213594@utp.edu.pe>
2025-08-25 07:51:00 -0500

    Add files via upload

---
+++ Cursos.java
@@ -0,0 +1,44 @@
+public class Cursos {
+      private String cursos;
+    private double tiempo; 
+    private String trabajos;
+    private String examenes;
+    public Cursos(String cursos, double tiempo, String trabajos, String examenes) {
+        this.cursos = cursos;
+        this.tiempo = tiempo;
+        this.trabajos = trabajos;
+        this.examenes = examenes;
+    }
+
+    public String getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(String cursos) {
+        this.cursos = cursos;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getTrabajos() {
+        return trabajos;
+    }
+
+    public void setTrabajos(String trabajos) {
+        this.trabajos = trabajos;
+    }
+
+    public String getExamenes() {
+        return examenes;
+    }
+
+    public void setExamenes(String examenes) {
+        this.examenes = examenes;
+    }
+}
